c3295c61accfc00e21257b799a4c2fc11e845e0f
milanchauhan1 <160685631+milanchauhan1@users.noreply.github.com>
2026-02-16 18:52:17 -0600

    Song class made constructors variables get sets allat
    dont really know how to do the format thing right yet still working on that

--- U4BLab/src/Playlist.java
+++ U4BLab/src/Playlist.java
@@ -1,2 +1,5 @@
+import java.util.ArrayList;
+
 public class Playlist {
+    private ArrayList<String> songs = new ArrayList<String>();
 }
--- U4BLab/src/Song.java
+++ U4BLab/src/Song.java
@@ -1,2 +1,70 @@
 public class Song {
+    private String title;
+    private String artist;
+    private String album;
+    private int durationSeconds;
+    private int releaseYear;
+    private String genre;
+
+
+    public Song(String title, String artist, String album, int durationSeconds, int releaseYear, String genre){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.durationSeconds = durationSeconds;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+
+    }
+    public String toString(){
+        return String.format("%-10s %-15s %-20s %-25d %-30s" , title, artist, album, releaseYear, genre);
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setDurationSeconds(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
